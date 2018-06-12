afbea1ae4d268b05b6aedf3e7b1468a4c2834502
InkoHX <inkohx@gmail.com>
2018-06-12 14:41:30 +0900

    間違えた()

--- Games_Core/src/Core/Commands/gamehelp.php
+++ Games_Core/src/Core/Commands/gamehelp.php
@@ -35,7 +35,7 @@ class gamehelp extends Command
     public function execute(CommandSender $sender, string $commandLabel, array $args) : bool
     {
         if ($sender instanceof Player) {
-            $helpmeaage = ["type" => "modal", "title" => "§cルール", "content" => "1. MCPEPROXY、ModPE、LuaScriptなどを使ったチート(Hack)の使用は禁止です。見かけた場合すぐにBANします。\n2. 無差別に色んな所でこのサーバを宣伝する事\n3. 不適切な内容が含まれた文を送信・意味不明な文章を連続送信する事も禁止です。\n4. プレイヤー名に不適切な内容を含めて参加する事は禁止です。(名前の変更が確認できたらBANを解除します。)\n5. バグ・ラグの悪用は禁止です。\nこれらを全て理解した上で同意ボタンを押して下さい。", "button1" => "同意する", "button2" => "同意しない"];
+            $helpmeaage = [];
             $packet = new ModalFormRequestPacket();
             $packet->formId = 15674;
             $packet->formData = json_encode($helpmeaage);

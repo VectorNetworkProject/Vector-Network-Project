c217d62fb39ab18a1b1c6387d00b8a120eab1364
InkoHX <inkohx@gmail.com>
2018-06-16 11:16:58 +0900

    コマンド部分を修正(一部直ってない)
    php-cs-fixerでコードを綺麗に
    
    bug
    1. gamehelpを実行するとusageが出てくる

--- Games_Core/src/Core/Commands/gamehelp.php
+++ Games_Core/src/Core/Commands/gamehelp.php
@@ -11,19 +11,25 @@ namespace Core\Commands;
 use Core\{
     Main
 };
-use pocketmine\{
-    command\Command, command\CommandSender, network\mcpe\protocol\ModalFormRequestPacket, Player, utils\TextFormat
-};
+use pocketmine\command\CommandSender;
+use pocketmine\command\PluginCommand;
+use pocketmine\network\mcpe\protocol\ModalFormRequestPacket;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat;
 
-class gamehelp extends Command
+class gamehelp extends PluginCommand
 {
+    private $plugin;
     /**
      * gamehelp constructor.
      * @param Main $plugin
      */
     public function __construct(Main $plugin)
     {
-        parent::__construct("gamehelp", "Vector Network のコマンド一覧を表示します。", "使い方: /gamehelp");
+        parent::__construct("gamehelp", $plugin);
+        $this->setPermission("vector.network.player");
+        $this->setDescription("Vector Network で使えるコマンド一覧を表示します。");
+        $this->plugin = $plugin;
     }
 
     /**
@@ -34,16 +40,21 @@ class gamehelp extends Command
      */
     public function execute(CommandSender $sender, string $commandLabel, array $args) : bool
     {
+        if (!$this->plugin->isEnabled()) {
+            return false;
+        }
+        if (!$this->testPermission($sender)) {
+            return false;
+        }
         if ($sender instanceof Player) {
-            $helpmeaage = [];
-            $packet = new ModalFormRequestPacket();
-            $packet->formId = 15674;
-            $packet->formData = json_encode($helpmeaage);
-            $sender->dataPacket($packet);
-            return true;
-        } else {
-            $sender->sendMessage(TextFormat::RED."このコマンドはゲーム内で実行して下さい。");
+            $message = [];
+            /* $packet = new ModalFormRequestPacket();
+            $packet->formId = 156461;
+            $packet->formData = json_decode($message, true); */
+            $sender->sendMessage("test");
             return true;
         }
+        $this->plugin->getLogger()->info(TextFormat::RED."このコマンドはプレイヤーのみが実行できます。");
+        return true;
     }
 }

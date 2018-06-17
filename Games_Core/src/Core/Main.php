c3e3c7efa51d648ab03bda190a95631041461d53
InkoHX <inkohx@gmail.com>
2018-06-17 15:47:51 +0900

    AdChatの廃止
    また今度付けるわ

--- Games_Core/src/Core/Main.php
+++ Games_Core/src/Core/Main.php
@@ -9,55 +9,30 @@
 namespace Core;
 
 use Core\{
-    Checker\PlayerAddressChecker, Commands\adchat, Commands\gamehelp
+    Commands\gamehelp
 };
 
 use pocketmine\{
-    event\Listener,
-    event\player\PlayerJoinEvent,
-    event\player\PlayerPreLoginEvent,
-    event\player\PlayerQuitEvent,
     plugin\PluginBase,
     utils\TextFormat
 };
 
-class Main extends PluginBase implements Listener
+class Main extends PluginBase
 {
     public static $instance = null;
     public function onEnable()
     {
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
         $this->getLogger()->info(TextFormat::GREEN."Games_Coreを読み込みました。");
     }
     public function onDisable()
     {
         $this->getLogger()->info(TextFormat::RED."Games_Coreを終了しました。");
     }
-    public function onPreLogin(PlayerPreLoginEvent $event)
-    {
-        $player = $event->getPlayer();
-        $check = new PlayerAddressChecker();
-        if ($check->Checker($player->getAddress())) {
-            $this->getLogger()->info($player->getName()."は国内からのアクセスです。");
-        } else {
-            $player->kick("§l§6Vector §bNetwork\n§r§fあなたはサーバーからキックされました。\n§7理由: §f国外からのアクセス", false);
-            $this->getLogger()->info($player->getName()."は国外からのアクセスの為キックしました。");
-        }
-    }
-    public function onJoin(PlayerJoinEvent $event)
-    {
-        $event->setJoinMessage(null);
-    }
-    public function onQuit(PlayerQuitEvent $event)
-    {
-        $event->setQuitMessage(null);
-    }
-
     private function registerCommands()
     {
         $commands = [
-            new gamehelp($this),
-            new adchat($this)
+            new gamehelp($this)
        ];
         $this->getServer()->getCommandMap()->registerAll($this->getName(), $commands);
     }

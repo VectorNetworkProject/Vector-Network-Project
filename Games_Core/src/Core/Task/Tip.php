27f7572c4104e814dd7aa70c52440e53bf4e36a3
InkoHX <inkohx@gmail.com>
2018-07-17 14:06:25 +0900

    update

--- Games_Core/src/Core/Task/Tip.php
+++ Games_Core/src/Core/Task/Tip.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: PCink
+ * User: InkoHX
  * Date: 2018/07/17
  * Time: 13:10
  */
@@ -19,7 +19,7 @@ class Tip extends PluginTask
     {
         $rand = mt_rand(1, 3);
         if ($rand === 1) {
-            $this->owner->getServer()->broadcastMessage("§b[§7TIP§b] §7test1");
+            $this->owner->getServer()->broadcastMessage("§b[§7TIP§b] §7このゲームはまだ未完成です。");
         } elseif ($rand === 2) {
             $this->owner->getServer()->broadcastMessage("§b[§7TIP§b] §7test2");
         } elseif ($rand === 3) {

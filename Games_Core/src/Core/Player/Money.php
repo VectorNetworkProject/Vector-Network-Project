832764e331ca84d54068dec0595a4886201f9aa3
InkoHX <inkohx@gmail.com>
2018-07-19 09:53:00 +0900

    ゲーム内通貨関係
    お金の取得、お金のセット、お金を減らす関数を追加

---
+++ Games_Core/src/Core/Player/Money.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: InkoHX
+ * Date: 2018/07/19
+ * Time: 9:20
+ */
+
+namespace Core\Player;
+
+
+use Core\DataFile;
+
+class Money
+{
+    /**
+     * @param string $name
+     * @return int
+     */
+    public function getMoney(string $name) : int {
+        $datafile = new DataFile($name);
+        $data = $datafile->get('USERDATA');
+        $Money = $data['money'];
+        return $Money;
+    }
+
+    /**
+     * @param string $name
+     * @param int $money
+     */
+    public function setMoney(string $name, int $money) {
+        $datafile = new DataFile($name);
+        $data = $datafile->get('USERDATA');
+        $data['money'] = $money;
+        $datafile->write('USERDATA', $data);
+    }
+
+    /**
+     * @param string $name
+     * @param int $money
+     * @return bool
+     */
+    public function reduceMoney(string $name, int $money) : bool {
+        $datafile = new DataFile($name);
+        $data = $datafile->get('USERDATA');
+        if ($data['money'] < $money) {
+            return false;
+        } else {
+            $data['money'] = $data['money'] - $money;
+            $datafile->write('USERDATA', $data);
+            return true;
+        }
+    }
+}

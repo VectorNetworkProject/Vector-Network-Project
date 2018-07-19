d6b87de24fe7e9485d47f6f07ed6bdc4c32e90a8
InkoHX <inkohx@gmail.com>
2018-07-19 10:16:29 +0900

    Level関係

--- Games_Core/src/Core/Player/Level.php
+++ Games_Core/src/Core/Player/Level.php
@@ -9,7 +9,103 @@
 namespace Core\Player;
 
 
+use Core\DataFile;
+
 class Level
 {
+    /**
+     * @param string $name
+     * @return int
+     */
+    public function getLevel(string $name) : int {
+        $datafile = new DataFile($name);
+        $data = $datafile->get('USERDATA');
+        return $data['networklevel'];
+    }
 
+    /**
+     * @param string $name
+     * @return int
+     */
+    public function getExp(string $name) : int {
+        $datafile = new DataFile($name);
+        $data = $datafile->get('USERDATA');
+        return $data['exp'];
+    }
+
+    /**
+     * @param string $name
+     * @return int
+     */
+    public function getMaxExp(string $name) : int {
+        $datafile = new DataFile($name);
+        $data = $datafile->get('USERDATA');
+        return $data['maxexp'];
+    }
+
+    /**
+     * @param string $name
+     * @param int $level
+     */
+    public function setLevel(string $name, int $level) {
+        $datafile = new DataFile($name);
+        $data = $datafile->get('USERDATA');
+        $data['networklevel'] = $level;
+        $datafile->write('USERDATA', $data);
+    }
+
+    /**
+     * @param string $name
+     * @param int $exp
+     */
+    public function setExp(string $name, int $exp) {
+        $datafile = new DataFile($name);
+        $data = $datafile->get('USERDATA');
+        $data['exp'] = $exp;
+        $datafile->write('USERDATA', $data);
+    }
+
+    /**
+     * @param string $name
+     * @param int $maxexp
+     */
+    public function setMaxExp(string $name, int $maxexp) {
+        $datafile = new DataFile($name);
+        $data = $datafile->get('USERDATA');
+        $data['maxexp'] = $maxexp;
+        $datafile->write('USERDATA', $data);
+    }
+
+    /**
+     * @param string $name
+     * @param int $level
+     */
+    public function addLevel(string $name, int $level) {
+        $datafile = new DataFile($name);
+        $data = $datafile->get('USERDATA');
+        $data['networklevel'] += $level;
+        $datafile->write('USERDATA', $data);
+    }
+
+    /**
+     * @param string $name
+     * @param int $exp
+     */
+    public function addExp(string $name, int $exp) {
+        $datafile = new DataFile($name);
+        $data = $datafile->get('USERDATA');
+        $data['exp'] += $exp;
+        $datafile->write('USERDATA', $data);
+    }
+
+    /**
+     * @param string $name
+     * @param int $maxexp
+     */
+    public function addMaxExp(string $name, int $maxexp) {
+        $datafile = new DataFile($name);
+        $data = $datafile->get('USERDATA');
+        $data['maxexp'] += $maxexp;
+        $datafile->write('USERDATA', $data);
+    }
 }

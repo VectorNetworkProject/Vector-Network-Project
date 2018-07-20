10cf922cc364fa12628507822ca3280502547c72
MazaiCrafty <takatim1574@icloud.com>
2018-07-20 22:37:36 +0900

    smarty code

--- Games_Core/src/Core/Player/Tag.php
+++ Games_Core/src/Core/Player/Tag.php
@@ -33,6 +33,28 @@ class Tag
     const WHITE = 15;
     const NO_COLLOR = 16;
 
+    private static $colors = [];
+
+    public static function registerColors(){
+        Tag::$colors[Tag::BLACK] = "§0$tag";
+        Tag::$colors[Tag::DARK_BLUE] = "§1$tag";
+        Tag::$colors[Tag::DARK_GREEN] = "§2$tag";
+        Tag::$colors[Tag::DARK_AQUA] = "§3$tag";
+        Tag::$colors[Tag::DARK_RED] = "§4$tag";
+        Tag::$colors[Tag::PURPLE] = "§5$tag";
+        Tag::$colors[Tag::GOLD] = "§6$tag";
+        Tag::$colors[Tag::GRAY] = "§7$tag";
+        Tag::$colors[Tag::DARK_GRAY] = "§8$tag";
+        Tag::$colors[Tag::BLUE] = "§9$tag";
+        Tag::$colors[Tag::LIGHT_GREEN] = "§a$tag";
+        Tag::$colors[Tag::AQUA] = "§b$tag";
+        Tag::$colors[Tag::RED] = "§c$tag";
+        Tag::$colors[Tag::PINK] = "§d$tag";
+        Tag::$colors[Tag::YELLOW] = "§e$tag";
+        Tag::$colors[Tag::WHITE] = "§f$tag";
+        Tag::$colors[Tag::NO_COLLOR] = "$tag";
+    }
+
     /**
      * @param Player $player
      * @return mixed
@@ -57,65 +79,17 @@ class Tag
             $player->sendMessage("§7[§c失敗§7] §cタグは8文字以内にして下さい");
             return;
         }
-        switch ($colorid) {
-            case Tag::BLACK:
-                $data['tag'] = "§0$tag";
-                break;
-            case Tag::DARK_BLUE:
-                $data['tag'] = "§1$tag";
-                break;
-            case Tag::DARK_GREEN:
-                $data['tag'] = "§2$tag";
-                break;
-            case Tag::DARK_AQUA:
-                $data['tag'] = "§3$tag";
-                break;
-            case Tag::DARK_RED:
-                $data['tag'] = "§4$tag";
-                break;
-            case Tag::PURPLE:
-                $data['tag'] = "§5$tag";
-                break;
-            case Tag::GOLD:
-                $data['tag'] = "§6$tag";
-                break;
-            case Tag::GRAY:
-                $data['tag'] = "§7$tag";
-                break;
-            case Tag::DARK_GRAY:
-                $data['tag'] = "§8$tag";
-                break;
-            case Tag::BLUE:
-                $data['tag'] = "§9$tag";
-                break;
-            case Tag::LIGHT_GREEN:
-                $data['tag'] = "§a$tag";
-                break;
-            case Tag::AQUA:
-                $data['tag'] = "§b$tag";
-                break;
-            case Tag::RED:
-                $data['tag'] = "§c$tag";
-                break;
-            case Tag::PINK:
-                $data['tag'] = "§d$tag";
-                break;
-            case Tag::YELLOW:
-                $data['tag'] = "§e$tag";
-                break;
-            case Tag::WHITE:
-                $data['tag'] = "§f$tag";
-                break;
-            case Tag::NO_COLLOR:
-                $data['tag'] = "$tag";
-                break;
-            default:
-                $data['tag'] = "$tag";
-                $player->sendMessage("§7[§cエラー§7] 指定したカラーIDが見つからなかった為デフォルトの色にしました。");
-                break;
+
+        if ((0 <= $colorid) && ($colorid <= 16)){
+            $data['tag'] = Tag::$colors[$colorid];
+            $message = "§7[§a成功§7] §7あなたのタグを【 $usertag §7】に設定しました。";
+        } else {
+            $data['tag'] = "$tag";
+            $message = "§7[§cエラー§7] 指定したカラーIDが見つからなかった為デフォルトの色にしました。";
         }
+
         $datafile->write('USERDATA', $data);
         $usertag = $data['tag'];
-        $player->sendMessage("§7[§a成功§7] §7あなたのタグを【 $usertag §7】に設定しました。");
+        $player->sendMessage($message);
     }
 }

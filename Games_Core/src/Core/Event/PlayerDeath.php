4adfa2d71f7252fe20148fb6f1393c4b699939fd
InkoHX <inkohx@gmail.com>
2018-07-22 16:52:44 +0900

    Update
    KillerのMaxハートを1上げ金リンゴを与える

--- Games_Core/src/Core/Event/PlayerDeath.php
+++ Games_Core/src/Core/Event/PlayerDeath.php
@@ -12,6 +12,7 @@ use Core\Game\FFAPvP\FFAPvPCore;
 use Core\Main;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\item\Item;
 use pocketmine\Player;
 
 class PlayerDeath
@@ -29,11 +30,15 @@ class PlayerDeath
         $player = $event->getPlayer();
         $cause = $player->getLastDamageCause();
         if ($player->getLevel()->getName() === "ffapvp") {
+            $event->setDrops(null);
+            $player->setMaxHealth(20);
             $this->ffapvp->AddDeathCount($player);
             if ($cause instanceof EntityDamageByEntityEvent) {
                 $damager = $cause->getDamager();
                 if ($damager instanceof Player) {
                     $this->ffapvp->AddKillCount($damager);
+                    $damager->setMaxHealth($damager->getMaxHealth() + 1);
+                    $damager->getInventory()->addItem(Item::get(Item::GOLDEN_APPLE, 0, 1));
                 }
             }
         }

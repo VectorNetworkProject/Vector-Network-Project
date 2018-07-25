170eeef018ea9522330a191f2d5545963144b912
InkoHX <inkohx@gmail.com>
2018-07-25 12:31:21 +0900

    test commit

--- Games_Core/src/Core/Game/SpeedCorePvP/SpeedCorePvPCore.php
+++ Games_Core/src/Core/Game/SpeedCorePvP/SpeedCorePvPCore.php
@@ -9,7 +9,22 @@
 namespace Core\Game\SpeedCorePvP;
 
 
+use Core\Main;
+use pocketmine\Player;
+use pocketmine\utils\Color;
+
 class SpeedCorePvPCore
 {
+    protected $plugin;
+    protected $bluecolor;
+    protected $redcolor;
+    public function __construct(Main $plugin)
+    {
+        $this->plugin = $plugin;
+        $this->bluecolor = new Color(0, 0, 255);
+        $this->redcolor = new Color(255, 0, 0);
+    }
+    public function GameJoin(Player $player) {
 
+    }
 }

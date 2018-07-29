ae6afc0cbd17b4451da27d13e1a067642840998f
InkoHX <inkohx@gmail.com>
2018-07-29 20:31:22 +0900

    初参加メッセージを実装

--- Games_Core/src/Core/Event/PlayerLogin.php
+++ Games_Core/src/Core/Event/PlayerLogin.php
@@ -41,6 +41,7 @@ class PlayerLogin
 				"lastlogin" => date("Y年m月d日 H時i分s秒")
 			];
 			$data->write("USERDATA", $user);
+			$this->plugin->getServer()->broadcastMessage("§7[§b情報§7] $name は初参加です。");
 		}
 		if (($ffapvp = $data->get("FFAPVP")) === null) {
 			$ffapvp = [

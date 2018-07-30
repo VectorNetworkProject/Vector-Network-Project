9424ef3b9024e1836c8b8a05b16dfaedada7a0aa
InkoHX <inkohx@gmail.com>
2018-07-30 10:50:10 +0900

    FFAPvPの装備に弓を追加

--- Games_Core/src/Core/Game/FFAPvP/FFAPvPCore.php
+++ Games_Core/src/Core/Game/FFAPvP/FFAPvPCore.php
@@ -71,7 +71,8 @@ class FFAPvPCore
 					"leather_tunic" => Item::get(Item::LEATHER_TUNIC, 0, 1),
 					"leather_pants" => Item::get(Item::LEATHER_PANTS, 0, 1),
 					"leather_boots" => Item::get(Item::LEATHER_BOOTS, 0, 1),
-					"wooden_sword" => Item::get(Item::WOODEN_SWORD, 0, 1)
+					"wooden_sword" => Item::get(Item::WOODEN_SWORD, 0, 1),
+					"bow" => Item::get(Item::BOW, 0, 1)
 				];
 				foreach ($items as $item) {
 					if ($item instanceof Durable) {
@@ -84,7 +85,9 @@ class FFAPvPCore
 				$armor->setLeggings($items["leather_pants"]);
 				$armor->setBoots($items["leather_boots"]);
 				$player->getInventory()->addItem($items["wooden_sword"]);
+				$player->getInventory()->addItem($items["bow"]);
 				$player->getInventory()->addItem(Item::get(Item::STEAK, 0, 64));
+				$player->getInventory()->addItem(Item::get(Item::ARROW, 0, 64));
 				$player->sendMessage("§a初期装備を与えました。テスト公開ですがお楽しみください");
 			}
 		}

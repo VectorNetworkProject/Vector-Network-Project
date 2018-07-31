cfcdcef0e9927f8be4cca6589c1b01c7f3a0a35f
InkoHX <inkohx@gmail.com>
2018-07-31 21:54:40 +0900

    Fix

--- Games_Core/src/Core/Task/RemoveItemTask.php
+++ Games_Core/src/Core/Task/RemoveItemTask.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: PCink
+ * User: InkoHX
  * Date: 2018/07/31
  * Time: 14:28
  */
@@ -9,8 +9,7 @@
 namespace Core\Task;
 
 
-use pocketmine\entity\Creature;
-use pocketmine\item\Item;
+use pocketmine\entity\object\ItemEntity;
 use pocketmine\plugin\Plugin;
 
 class RemoveItemTask extends PluginTask
@@ -23,8 +22,8 @@ class RemoveItemTask extends PluginTask
 	public function onRun(int $currentTick)
 	{
 		foreach ($this->owner->getServer()->getLevelByName("corepvp")->getEntities() as $entity) {
-			if ($entity instanceof Creature) {
-				$entity->close();
+			if ($entity instanceof ItemEntity) {
+				$entity->kill();
 			}
 		}
 	}

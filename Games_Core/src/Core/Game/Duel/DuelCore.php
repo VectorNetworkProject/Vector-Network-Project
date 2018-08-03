58a9a8c50bc2fa845ac5f954831331973a1588a0
InkoHX <inkohx@gmail.com>
2018-08-03 21:00:42 +0900

    Update duel

---
+++ Games_Core/src/Core/Game/Duel/DuelCore.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: InkoHX
+ * Date: 2018/08/03
+ * Time: 20:55
+ */
+
+namespace Core\Game\Duel;
+
+
+class DuelCore
+{
+	public function __construct()
+	{
+	}
+}

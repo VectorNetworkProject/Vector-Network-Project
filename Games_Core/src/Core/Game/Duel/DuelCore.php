c4c046bdd797a64f998b5648b044f71114aa830b
InkoHX <inkohx@gmail.com>
2018-08-04 12:10:10 +0900

    思考中

--- Games_Core/src/Core/Game/Duel/DuelCore.php
+++ Games_Core/src/Core/Game/Duel/DuelCore.php
@@ -9,9 +9,30 @@
 namespace Core\Game\Duel;
 
 
+use Core\Player\Level;
+use Core\Player\Money;
+use pocketmine\Player;
+
 class DuelCore
 {
+	protected $money;
+	protected $level;
+	protected $players = [];
+	protected $playerstatus = [];
+
 	public function __construct()
 	{
+		$this->money = new Money();
+		$this->level = new Level();
+	}
+
+	public function EndGame(Player $player, int $type)
+	{
+		switch ($type) {
+			case 1:
+				break;
+			case 2:
+				break;
+		}
 	}
 }

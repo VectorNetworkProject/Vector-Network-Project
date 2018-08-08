87ef5cb6dfd8feedc3442508af130c6ec3f2db54
InkoHX <inkohx@gmail.com>
2018-08-08 14:03:15 +0900

    Fix Task

--- Games_Core/src/Core/Main.php
+++ Games_Core/src/Core/Main.php
@@ -57,7 +57,7 @@ class Main extends PluginBase
 		$this->getScheduler()->scheduleRepeatingTask(new Tip($this), 180 * 20);
 		$this->getScheduler()->scheduleRepeatingTask(new AutoSavingTask($this), 10 * 20);
 		$this->getScheduler()->scheduleRepeatingTask(new RemoveItemTask($this), 30 * 20);
-		$this->getScheduler()->scheduleRepeatingTask(new FoodTask($this), 30 * 20);
+		$this->getScheduler()->scheduleRepeatingTask(new FoodTask($this), 10 * 20);
 		$this->getServer()->loadLevel("ffapvp");
 		$this->getServer()->loadLevel("corepvp");
 		$this->getServer()->loadLevel("athletic");

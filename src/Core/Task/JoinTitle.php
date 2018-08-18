d4cb0ad6f0eb6bd35d76b3d83ceba3a86512a145
InkoHX <inkohx@gmail.com>
2018-08-18 11:44:02 +0900

    Add Change message
    変更内容を取得して送信

--- src/Core/Task/JoinTitle.php
+++ src/Core/Task/JoinTitle.php
@@ -10,6 +10,9 @@ namespace Core\Task;
 
 use pocketmine\Player;
 use pocketmine\plugin\Plugin;
+use pocketmine\utils\Internet;
+use tokyo\pmmp\libform\element\Label;
+use tokyo\pmmp\libform\FormApi;
 
 class JoinTitle extends PluginTask
 {
@@ -38,5 +41,28 @@ class JoinTitle extends PluginTask
 		$player = $this->player;
 		$player->addTitle("§6Vector §bNetwork", "§eDeveloped by InkoHX MazaiCrafty", 40, 100, 40);
 		$player->sendMessage("§a---===< §6Vector §bNetwork §eProject §a>===---\n§bDeveloped by InkoHX MazaiCrafty\n§bGitHub: §7https://github.com/InkoHX/Vector-Network-Project\n§bTwitter: §7https://twitter.com/InkoHX\n§9Discord: §7https://discord.gg/EF2G5dh\n§a---=============================---");
+		$custom = FormApi::makeCustomForm(function ($response) {
+			if (!FormApi::formCancelled($response)) {}
+		});
+		$custom->setTitle("変更内容")
+			->addElement(new Label("ゲームバージョン: " . self::getVersion() . "\n" . self::getChange()))
+			->setId(mt_rand(111111, 999999))
+			->sendToPlayer($player);
+	}
+
+	/**
+	 * @return bool|mixed
+	 */
+	private static function getChange()
+	{
+		return Internet::getURL('https://vnp.inkohx.xyz/log/change.txt');
+	}
+
+	/**
+	 * @return bool|mixed
+	 */
+	private static function getVersion()
+	{
+		return Internet::getURL('https://vnp.inkohx.xyz/log/game_version.txt');
 	}
 }

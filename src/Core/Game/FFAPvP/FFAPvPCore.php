73a7db01872ae82afc8e1bf0a75e21bd545f022c
InkoHX <inkohx@gmail.com>
2018-08-19 20:11:40 +0900

    Update FFAPvP

--- src/Core/Game/FFAPvP/FFAPvPCore.php
+++ src/Core/Game/FFAPvP/FFAPvPCore.php
@@ -68,8 +68,8 @@ class FFAPvPCore
 				$player->setFood(20);
 				$items = [
 					"leather_cap" => Item::get(Item::LEATHER_CAP, 0, 1),
-					"leather_tunic" => Item::get(Item::LEATHER_TUNIC, 0, 1),
-					"leather_pants" => Item::get(Item::LEATHER_PANTS, 0, 1),
+					"chain_tunic" => Item::get(Item::CHAIN_CHESTPLATE, 0, 1),
+					"chain_leggings" => Item::get(Item::CHAIN_LEGGINGS, 0, 1),
 					"leather_boots" => Item::get(Item::LEATHER_BOOTS, 0, 1),
 					"wooden_sword" => Item::get(Item::WOODEN_SWORD, 0, 1),
 					"bow" => Item::get(Item::BOW, 0, 1)
@@ -81,14 +81,14 @@ class FFAPvPCore
 				}
 				$armor = $player->getArmorInventory();
 				$armor->setHelmet($items["leather_cap"]);
-				$armor->setChestplate($items["leather_tunic"]);
-				$armor->setLeggings($items["leather_pants"]);
+				$armor->setChestplate($items["chain_tunic"]);
+				$armor->setLeggings($items["chain_leggings"]);
 				$armor->setBoots($items["leather_boots"]);
 				$player->getInventory()->addItem($items["wooden_sword"]);
 				$player->getInventory()->addItem($items["bow"]);
 				$player->getInventory()->addItem(Item::get(Item::STEAK, 0, 64));
 				$player->getInventory()->addItem(Item::get(Item::ARROW, 0, 64));
-				$player->sendMessage("§a初期装備を与えました。テスト公開ですがお楽しみください");
+				$player->sendMessage("§a初期装備を与えました。");
 			}
 		}
 	}

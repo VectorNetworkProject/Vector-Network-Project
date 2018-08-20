135184aebdd0447981a00cd27b45a01a5d113849
InkoHX <inkohx@gmail.com>
2018-08-20 16:45:23 +0900

    Add tip message

--- src/Core/Task/MOTDTip.php
+++ src/Core/Task/MOTDTip.php
@@ -20,13 +20,22 @@ class MOTDTip extends PluginTask
 
 	public function onRun(int $currentTick)
 	{
-		$rand = mt_rand(1, 2);
+		$rand = mt_rand(1, 5);
 		switch ($rand) {
 			case 1:
 				$this->owner->getServer()->getNetwork()->setName("§l§6>§e>§6> §a3GAMES §6<§e<§6<§7");
 				break;
+			case 2:
+				$this->owner->getServer()->getNetwork()->setName("§l§6>§e>§6> §aSURVIVAL §6<§e<§6<§7");
+				break;
+			case 3:
+				$this->owner->getServer()->getNetwork()->setName("§l§6>§e>§6> §bSpeed§aCore§cPvP §6<§e<§6<§7");
+				break;
+			case 4:
+				$this->owner->getServer()->getNetwork()->setName("§l§6>§e>§6> §6FFA§cPvP §6<§e<§6<§7");
+				break;
 			default:
-				$this->owner->getServer()->getNetwork()->setName("§6Vector §bNetwork §eProject§7");
+				$this->owner->getServer()->getNetwork()->setName("§l§6Vector §bNetwork §eProject§7");
 				break;
 		}
 	}

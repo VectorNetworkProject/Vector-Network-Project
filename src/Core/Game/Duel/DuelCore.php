479cedc4f1951654d8772ae0a3d05958ab6902ed
InkoHX <inkohx@gmail.com>
2018-08-26 21:40:42 +0900

    fix

--- src/Core/Game/Duel/DuelCore.php
+++ src/Core/Game/Duel/DuelCore.php
@@ -11,8 +11,10 @@ namespace Core\Game\Duel;
 
 use Core\DataFile;
 use Core\Main;
+use Core\Player\Level;
+use Core\Player\Money;
 use Core\Task\Teleport\TeleportDuelStageTask;
-use pocketmine\event\entity\EntityLevelChangeEvent;
+use Core\Game\Duel\stage\Stage1;
 use pocketmine\event\player\PlayerCommandPreprocessEvent;
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\event\player\PlayerQuitEvent;
@@ -21,64 +23,149 @@ use pocketmine\Player;
 
 class DuelCore
 {
+
 	const DUEL_MAX_PLAYER = 2;
-	const DUEL_PLAYER = 0;
 	const LEVEL_NAME = "duel";
 
-	protected $player = 0;
-	protected $joined = [];
-	protected $status = false;
+	protected $stages = [];
+	protected $datas = [];
+	protected $players = [];
+	protected $playerslist = [];//Player Class
+	protected $status = [];
+	protected $stage, $usedStage;
+	public static $gameId = 0, $money, $level;
 
-	public function DuelJoin(Player $player)
+	public function __construct()
 	{
+		$this->stages = [
+			new Stage1(),
+			//new Stage2(),
+			//new Stage3(),
+			//new Stage4(),
+			//new Stage5(),
+			//new Stage6(),
+		];
+		self::$money = new Money();
+		self::$level = new Level();
+	}
+
+	public function create(int $gameId)
+	{
+		$this->players[$gameId] = 0;
+		$this->playerslist[$gameId] = [];
+		$this->status[$gameId] = false;
+	}
+
+	public function DuelJoin(Player $player, int $gameId = 0)
+	{
+		$name = $player->getName();
 		if ($player->getLevel()->getName() === self::LEVEL_NAME) return;
-		if ($this->status === true) {
-			$player->sendMessage("§c現在ゲーム中です。");
-		}
-		if (isset($this->joined[$player->getName()])) {
-			$player->sendMessage("§c既にDuelに参加しています。");
-		} else {
-			if ($this->player === 1) {
-				$this->status = true;
+		if (!$this->status[$gameId]) {
+			if (empty($this->datas[$name])) {
+				$data["gameId"] = $gameId;
+				$this->datas[$name] = $data;
+				$player->sendMessage("§aDuelに参加しました。");
 				$player->setImmobile(true);
-				Main::$instance->getScheduler()->scheduleDelayedTask(new TeleportDuelStageTask(Main::$instance, $this->joined), 6*20);
+				$this->players[$gameId]++;
+				$this->playerslist[$gameId][$player->getId()] = $player;
+				if ($this->players[$gameId] == self::DUEL_MAX_PLAYER) {
+					$this->randStage($gameId);
+					$this->status[$gameId] = true;
+					$this->wattingGame($gameId);
+				}
+			} else {
+				$player->sendMessage("§c既にDuelに参加しています。");
 			}
-			$this->joined[$player->getName()] = $player->getName();
-			$player->sendMessage("§aDuelに参加しました。");
-			$player->setImmobile(true);
-			$this->player++;
+		} else {
+			$gameId = self::$gameId++;
+			$this->create($gameId);
+			$this->DuelJoin($player, $gameId);
+		}
+	}
+
+	public function DuelQuit(Player $player)
+	{
+		$gameId = self::getGameIdByPlayer($player);
+		if ($gameId == null) return;
+		if ($this->status[$gameId]) {
+			$this->endGame($player);
+		} else {
+			$name = $player->getName();
+			unset($this->datas[$name]);
+			unset($this->playerslist[$gameId][$name]);
+			$this->players[$gameId]--;
+			$player->setHealth(20);
+			$player->setFood(20);
+			$player->getInventory()->clearAll();
+			$player->teleport(new Position(257, 8, 257, Main::$instance->getServer()->getLevelByName(self::LEVEL_NAME)));
+		}
+	}
+
+	public function wattingGame(int $gameId)
+	{
+		$c = 0;
+		$spawnPosition = $this->stage[$gameId][1]->getSpawnPosition();
+		foreach ($this->playerslist[$gameId] as $entityId => $player) {
+			$player->teleport($spawnPosition[$c]);
+			$c++;
+		}
+		Main::$instance->getScheduler()->scheduleDelayedTask(new TeleportDuelStageTask(Main::$instance, $this, $gameId), 6 * 20);
+	}
+
+	public function startGame(int $gameId, int $count)
+	{
+		if ($count % 20 == 0) {
+			$time = 5 - $count % 20;
+			foreach ($this->playerslist[$gameId] as $entityId => $player) {
+				switch ($time) {
+					case 5:
+						$player->addTitle("§a5", "", 20, 20, 20);
+						break;
+					case 4:
+						$player->addTitle("§a4", "", 20, 20, 20);
+						break;
+					case 3:
+						$player->addTitle("§e3", "", 20, 20, 20);
+						break;
+					case 2:
+						$player->addTitle("§e2", "", 20, 20, 20);
+						break;
+					case 1:
+						$player->addTitle("§c1", "", 20, 20, 20);
+						break;
+					case 0:
+						$player->setImmobile(false);
+						$player->addTitle("§cSTART", "§cMode: Duel(1v1)", 20, 20, 20);
+						break;
+
+				}
+			}
 		}
 	}
 
 	public function endGame(Player $player)
 	{
-		if ($this->status === false) return;
-		foreach ($this->joined as $name) {
-			if ($name === $player->getName()) {
-				$player->addTitle("§cYOU DIED", "§cあなたは勝負に負けてしまった。", 20, 40, 20);
-				self::AddLose($name);
+		$gameId = self::getGameIdByPlayer($player);
+		if ($gameId == null) return;
+		if (!$this->status[$gameId]) return;
+		foreach ($this->playerslist[$gameId] as $entityId => $players) {
+			if ($player === $players) {
+				$players->addTitle("§cYOU DIED", "§cあなたは勝負に負けてしまった。", 20, 40, 20);
+				self::$level->LevelSystem($players);
+				self::AddLose($players);
 			} else {
-				Main::$instance->getServer()->getPlayer($name)->addTitle("§6VICTORY", "§6あなたは勝負に買った", 20, 40, 20);
-				self::sendMessage("§7[§cDuel§7] §e" . $name . "§aが勝利しました。");
-				self::AddWin($name);
+				$players->addTitle("§6VICTORY", "§6あなたは勝負に買った", 20, 40, 20);
+				$this->broatcastMessage($gameId, "§7[§cDuel§7] §e" . $player->getName() . "§aが勝利しました。");
+				self::$money->addMoney($players->getName(), 100);
+				self::$level->LevelSystem($players);
+				self::AddWin($players);
 			}
-			Main::$instance->getServer()->getPlayer($name)->setHealth(20);
-			Main::$instance->getServer()->getPlayer($name)->setFood(20);
-			Main::$instance->getServer()->getPlayer($name)->getInventory()->clearAll();
-			Main::$instance->getServer()->getPlayer($name)->teleport(new Position(257, 8, 257, Main::$instance->getServer()->getLevelByName(self::LEVEL_NAME)));
+			$players->setHealth(20);
+			$players->setFood(20);
+			$players->getInventory()->clearAll();
+			$players->teleport(new Position(257, 8, 257, Main::$instance->getServer()->getLevelByName(self::LEVEL_NAME)));
 		}
-		self::Reload();
-		unset($this->joined);
-		$this->status = false;
-		$this->player = 0;
-	}
-
-	public function Quit(PlayerQuitEvent $event)
-	{
-		$player = $event->getPlayer();
-		if (isset($this->joined[$player->getName()])) {
-			$this->endGame($player);
-		}
+		$this->reset($gameId);
 	}
 
 	public function Join(PlayerInteractEvent $event)
@@ -87,65 +174,80 @@ class DuelCore
 		$block = $event->getBlock();
 		if ($player->getLevel()->getName() !== self::LEVEL_NAME) return;
 		if ($block->getId() === 42) {
-			self::DuelJoin($player);
+			$this->DuelJoin($player);
+			//$this->DuelJoin($player, self::$gameId);
 		}
 	}
 
-	public function Leave(EntityLevelChangeEvent $event)
+	public function Quit(PlayerQuitEvent $event)
 	{
-		$entity = $event->getEntity();
-		if (!$entity instanceof Player) return;
-		if ($event->getOrigin()->getName() === self::LEVEL_NAME) {
-			unset($this->joined[$entity->getName()]);
+		$player = $event->getPlayer();
+		if (isset($this->datas[$player->getName()])) {
+			$this->DuelQuit($player);
 		}
 	}
 
 	public function CancelCommand(PlayerCommandPreprocessEvent $event)
 	{
-		if (isset($this->joined[$event->getPlayer()->getName()])) {
+		$player = $event->getPlayer();
+		if (isset($this->datas[$player->getName()])) {
 			if ($event->getMessage() === "/selectgame") return;
 		}
 	}
 
-	/**
-	 * @param string $name
-	 */
-	private static function AddWin(string $name)
+	public function randStage(int $gameId)
 	{
+		$rand = mt_rand(0, count($this->stages) - 1);
+		if (empty($this->usedStage[$rand])) {
+			$this->usedStage[$rand] = $gameId;
+			$this->stage[$gameId] = [$rand, $this->stages[$rand]];
+		} else {
+			$this->randStage($gameId);
+		}
+	}
+
+	public function reset(int $gameId)
+	{
+		foreach ($this->playerslist[$gameId] as $entityId => $player) {
+			unset($this->datas[$player->getName()]);
+		}
+		$this->playerslist[$gameId] = [];
+		$this->status[$gameId] = false;
+		$this->players[$gameId] = [];
+
+		$stageId = $this->stage[$gameId][0];
+		unset($this->usedStage[$stageId]);
+	}
+
+	public function broatcastMessage(int $gameId, string $message)
+	{
+		foreach ($this->playerslist[$gameId] as $entityId => $player) {
+			$player->sendMessage($message);
+		}
+	}
+
+	private static function AddWin(Player $player)
+	{
+		$name = $player->getName();
 		$datafile = new DataFile($name);
 		$data = $datafile->get('DUEL');
 		$data['win']++;
 		$datafile->write('DUEL', $data);
 	}
 
-	/**
-	 * @param string $name
-	 */
-	private static function AddLose(string $name)
+	private static function AddLose(Player $player)
 	{
+		$name = $player->getName();
 		$datafile = new DataFile($name);
 		$data = $datafile->get('DUEL');
 		$data['lose']++;
 		$datafile->write('DUEL', $data);
 	}
 
-	/**
-	 * @param string $message
-	 */
-	private static function sendMessage(string $message)
+	public function getGameIdByPlayer(Player $player)
 	{
-		foreach (Main::$instance->getServer()->getOnlinePlayers() as $player) {
-			if (!$player->getLevel()->getName() === self::LEVEL_NAME) return;
-			$player->sendMessage($message);
-		}
-	}
-
-	private static function Reload()
-	{
-		Main::$instance->getServer()->unloadLevel(Main::$instance->getServer()->getLevelByName(self::LEVEL_NAME));
-		Main::$instance->getServer()->loadLevel(self::LEVEL_NAME);
-		$level = Main::$instance->getServer()->getLevelByName(self::LEVEL_NAME);
-		$level->setTime(6000);
-		$level->stopTime();
+		$name = $player->getName();
+		if (empty($this->datas[$name])) return null;
+		return $this->datas[$name]["gameId"];
 	}
 }

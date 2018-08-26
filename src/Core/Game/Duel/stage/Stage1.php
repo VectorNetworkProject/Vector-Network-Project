b088f77b15e1183cc86958e3738715e5482ddf97
DusKong <2001kaito0506@ezweb.ne.jp>
2018-08-26 20:56:15 +0900

    Duel

---
+++ src/Core/Game/Duel/stage/Stage1.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Core\Game\Duel\stage;
+
+class Stage1 {
+
+    public function getSpawnPosition() {
+        return [
+            new Vector3(0,0,0),
+            new Vector3(0,0,0),
+        ];
+    }
+}
---
+++ src/Core/Task/Teleport/BTeleportDuelStageTask.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PCink
+ * Date: 2018/08/22
+ * Time: 13:10
+ */
+
+namespace Core\Task\Teleport;
+
+
+use Core\Main;
+use Core\Task\PluginTask;
+use Core\Game\Duel\DuelCore;
+use pocketmine\plugin\Plugin;
+
+class TeleportDuelStageTask extends PluginTask
+{
+    protected $game;
+    protected $gameId;
+    public function __construct(Plugin $plugin, DuelCore $game, int $gameId)
+    {
+        parent::__construct($plugin);
+        $this->game = $game;
+        $this->gameId = $gameId;
+    }
+    public function onRun(int $currentTick)
+    {
+        $this->game->startGame($this->gameId, $currentTick);
+    }
+}

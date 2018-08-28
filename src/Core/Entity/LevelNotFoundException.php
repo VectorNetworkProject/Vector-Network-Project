b4ae3e8a1f9ce97bcc8203e2ec8d2e04838a2cc1
UramnOIL <uramnoil@outlook.com>
2018-08-28 17:19:27 +0900

    LevelNotFoundExceptionの追加

---
+++ src/Core/Entity/LevelNotFoundException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: UramnOIL
+ * Date: 2018/08/28
+ * Time: 17:18
+ */
+
+namespace Core\Entity;
+
+
+use pocketmine\level\LevelException;
+
+class LevelNotFoundException extends LevelException
+{
+
+}

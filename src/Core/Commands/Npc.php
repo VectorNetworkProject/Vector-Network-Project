f3c25e3410e949bedb2c6024c7c313c8e984aa12
UramnOIL <uramnoil@outlook.com>
2018-08-28 20:18:57 +0900

    Npcコマンドの作成

--- src/Core/Commands/Npc.php
+++ src/Core/Commands/Npc.php
@@ -9,20 +9,47 @@
 namespace Core\Commands;
 
 
+use Core\Entity\VectorNPC\VectorNPCFactory;
+use Core\Main;
 use pocketmine\command\CommandSender;
 use pocketmine\command\PluginCommand;
-use pocketmine\plugin\Plugin;
 
 class Npc extends PluginCommand
 {
-	public function __construct(Plugin $owner)
+	private $factory;
+	public function __construct(Main $owner)
 	{
 		parent::__construct("npc", $owner);
 		$this->setPermission("vector.network.admin");
+		$this->factory = new VectorNPCFactory($owner);
 	}
 
 	public function execute(CommandSender $sender, string $commandLabel, array $args)
 	{
-		$
+		if(!$this->testPermissionSilent($sender))
+		{
+			return;
+		}
+		if (!isset($args[0])) {
+			$sender->sendMessage("NPCを指定してください");
+			return;
+		}
+		switch(strtolower($args[0])) {
+			case "gamemaster":
+				$this->factory->createGameMaster();
+				$sender->sendMessage("配置しました");
+				break;
+			case "mazai":
+				$this->factory->createMazai();
+				$sender->sendMessage("配置しました");
+				break;
+			case "mazaimaster":
+				$this->factory->createMazaiMaster();
+				$sender->sendMessage("配置しました");
+				break;
+			default:
+				$sender->sendMessage("NPCが存在しません");
+				break;
+		}
 	}
 }

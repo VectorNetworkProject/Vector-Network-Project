8448a87e35b8ef3f603421799e62b09ca056ce1b
UramnOIL <uramnoil@outlook.com>
2018-08-29 20:05:50 +0900

    PlayerDeathEvent

--- src/Core/Event/EventListener.php
+++ src/Core/Event/EventListener.php
@@ -33,6 +33,7 @@ use Core\Game\FFAPvP\FFAPvPCore;
 use Core\Game\SpeedCorePvP\SpeedCorePvPCore;
 use Core\Game\Survival\SurvivalCore;
 use Core\Main;
+use Core\Player\KillSound;
 use Core\Player\Level;
 use Core\Player\Rank;
 use Core\Player\Tag;
@@ -88,6 +89,7 @@ class EventListener implements Listener
 	protected $entityinventorychange;
 	protected $entityshootbowevent;
 	protected $playerexhaustevent;
+	private $killsound;
 
 	public function __construct(Main $plugin)
 	{
@@ -100,6 +102,7 @@ class EventListener implements Listener
 		$this->gamemasternpc = new GameMaster($this->plugin);
 		$this->mazaimasternpc = new MazaiMaster();
 		$this->survival = new SurvivalCore($this->plugin);
+		$this->killsound = new KillSound($this->plugin);
 	}
 
 	public function onJoin(PlayerJoinEvent $event)
@@ -212,7 +215,55 @@ class EventListener implements Listener
 
 	public function onDeath(PlayerDeathEvent $event)
 	{
-		$this->playerdeathevent->event($event);
+		$event->setDeathMessage(null);
+		$player = $event->getPlayer();
+		$cause = $player->getLastDamageCause();
+		if ($player->getLevel()->getName() === "ffapvp") {
+			$event->setDrops([Item::get(0, 0, 0)]);
+			$player->setMaxHealth(20);
+			if ($cause instanceof EntityDamageByEntityEvent) {
+				$damager = $cause->getDamager();
+				if ($damager instanceof Player) {
+					$this->ffapvp->AddKillCount($damager);
+					$this->ffapvp->AddDeathCount($player);
+					$this->DeathMessage('ffapvp', $player->getName(), $damager->getName());
+					if ($damager->getMaxHealth() < 40) {
+						$damager->setMaxHealth($damager->getMaxHealth() + 1);
+					}
+					$damager->getInventory()->addItem(Item::get(Item::GOLDEN_APPLE, 0, 1));
+					$this->killsound->PlaySound($damager);
+				}
+			} else {
+				$this->DeathMessage('ffapvp', $player->getName());
+				$this->ffapvp->AddDeathCount($player);
+			}
+		} elseif ($player->getLevel()->getName() === "corepvp") {
+			if ($cause instanceof EntityDamageByEntityEvent) {
+				$damager = $cause->getDamager();
+				if ($damager instanceof Player) {
+					$this->speedcorepvp->AddKillCount($damager);
+					$this->speedcorepvp->addDeathCount($player);
+					$this->killsound->PlaySound($damager);
+					$this->DeathMessage('corepvp', $player->getName(), $damager->getName());
+				}
+			} else {
+				$this->DeathMessage('corepvp', $player->getName());
+				$this->speedcorepvp->addDeathCount($player);
+			}
+		} elseif ($player->getLevel()->getName() === "Survival") {
+			if ($cause instanceof EntityDamageByEntityEvent) {
+				$damager = $cause->getDamager();
+				if ($damager instanceof Player) {
+					$this->survival->AddKillCount($damager);
+					$this->survival->AddDeathCount($player);
+					$this->killsound->PlaySound($damager);
+					$this->DeathMessage("Survival", $player->getName(), $damager->getName());
+				}
+			} else {
+				$this->DeathMessage("Survival", $player->getName());
+				$this->survival->AddDeathCount($player);
+			}
+		}
 	}
 
 	public function onReceive(DataPacketReceiveEvent $event)
@@ -327,5 +378,18 @@ class EventListener implements Listener
 	public function onPlayerExhaust(PlayerExhaustEvent $event)
 	{
 		$this->playerexhaustevent->event($event);
+	}
+
+	public function DeathMessage(string $levelname, string $killed = null, string $killer = null): void
+	{
+		foreach ($this->plugin->getServer()->getOnlinePlayers() as $player) {
+			if ($player->getLevel()->getName() === $levelname) {
+				if ($killer === null) {
+					$player->sendMessage("§7[§b情報§7] §c✖ §7$killed");
+				} else {
+					$player->sendMessage("§7[§b情報§7] §7$killer §c➡ §7$killed");
+				}
+			}
+		}
 	}
 }
